32a63e682fc488d4b28b27d1d4093f51df7bdc8b
leeprosser <lee.prosser@live.vu.edu.au>
2015-09-07 23:13:38 +1000

    modbus connection made

--- src/hello_world.c
+++ src/hello_world.c
@@ -7,52 +7,94 @@
 #include <arpa/inet.h>
 #include <netinet/in.h>
 #include <modbus-tcp.h>
-#define SERVER_ADDR "127.0.0.1"
+#define SERVER_ADDR "140.159.153.159"
 #define SERVER_PORT 10000
 #define DATA_LENGTH 256
 #define QUIT_STRING "exit"
+#include <errno.h>
 
-/* Linked list object */
+int16_t tab_reg[64];
+int errno;
+int i;
+int rc;
+int main(int argc, char *arg[])
+{
+while(1){modb();}
+}
+  
+int modb(void)
+{
+modbus_t *ctx;
+sleep(1);
+ctx = modbus_new_tcp("140.159.153.159", 502);
+if (modbus_connect(ctx) == -1) 
+   {
+   fprintf(stderr, "Connection failed: %s\n", modbus_strerror(errno));
+   modbus_free(ctx);
+   return -1;
+   }
+printf("conection not failed\n");
+rc = modbus_read_registers(ctx, 0, 3, tab_reg);
+if (rc == -1) 
+  {
+  if (EMBMDATA==1){printf("too many requests\n");}
+  printf("not able to read register\n");
+  fprintf(stderr, "%s\n", modbus_strerror(errno));
+  return -1;
+  }
+for (i=0; i < rc; i++) 
+  {
+  printf("reg[%d]=%d (0x%X)\n", i, tab_reg[i], tab_reg[i]);
+  }
+modbus_close(ctx);
+modbus_free(ctx);
+}
+
+/*  ALL CODE BELOW IS FOR FUTURE CONFIGURATION   */
+
+
+/* Linked list object*/
+/*
 typedef struct s_word_object word_object;
 struct s_word_object {
 char *word;
 word_object *next;
 };
-/* list_head: Shared between two threads, must be accessed with list_lock */
+/* list_head: Shared between two threads, must be accessed with list_lock
 static word_object *list_head;
 static pthread_mutex_t list_lock = PTHREAD_MUTEX_INITIALIZER;
 static pthread_cond_t list_data_ready = PTHREAD_COND_INITIALIZER;
 static pthread_cond_t list_data_flush = PTHREAD_COND_INITIALIZER;
-/* Add object to list */
+/* Add object to list 
 static void add_to_list(char *word) {
 word_object *last_object, *tmp_object;
 char *tmp_string;
 /* Do all memory allocation outside of locking - strdup() and malloc() can
  * * block */
-tmp_object = malloc(sizeof(word_object));
+/*tmp_object = malloc(sizeof(word_object));
 tmp_string = strdup(word);
 /* Set up tmp_object outside of locking */
-tmp_object->word = tmp_string;
+/*tmp_object->word = tmp_string;
 tmp_object->next = NULL;
 pthread_mutex_lock(&list_lock);
 if (list_head == NULL) {
 /* The list is empty, just place our tmp_object at the head */
-list_head = tmp_object;
+/*list_head = tmp_object;
 } else {
 /* Iterate through the linked list to find the last object */
-last_object = list_head;
+/*last_object = list_head;
 while (last_object->next) {
 last_object = last_object->next;
 }
 /* Last object is now found, link in our tmp_object at the tail */
-last_object->next = tmp_object;
+/*last_object->next = tmp_object;
 }
 pthread_mutex_unlock(&list_lock);
 pthread_cond_signal(&list_data_ready);
 }
 /* Retrieve the first object in the linked list. Note that this function must
  * * be called with list_lock held */
-static word_object *list_get_first(void) {
+/*static word_object *list_get_first(void) {
 word_object *first_object;
 first_object = list_head;
 list_head = list_head->next;
@@ -70,14 +112,14 @@ current_object = list_get_first();
 pthread_mutex_unlock(&list_lock);
 /* printf() and free() can block, make sure that we've released
  * * list_lock first */
-printf("Print thread: %s\n", current_object->word);
+/*printf("Print thread: %s\n", current_object->word);
 free(current_object->word);
 free(current_object);
 /* Let list_flush() know that we've done some work */
-pthread_cond_signal(&list_data_flush);
+/*pthread_cond_signal(&list_data_flush);
 }
 /* Silence compiler warning */
-return arg;
+/*return arg;
 }
 static void list_flush(void) {
 pthread_mutex_lock(&list_lock);
@@ -113,14 +155,14 @@ FD_ZERO(&read_fds);
 FD_SET(socket_fd, &read_fds);
 while (!want_quit) {
 /* Wait until data has arrived */
-if (select(socket_fd + 1, &read_fds, NULL, NULL, NULL) < 0) {
+/*if (select(socket_fd + 1, &read_fds, NULL, NULL, NULL) < 0) {
 fprintf(stderr, "Select failed\n");
 exit(1);
 }
 if (!FD_ISSET(socket_fd, &read_fds)) continue;
 /* Read input data */
 
-bytes = recvfrom(socket_fd, data, sizeof(data), 0,
+/*bytes = recvfrom(socket_fd, data, sizeof(data), 0,
 (struct sockaddr *) &client_address,
 &client_address_len);
 if (bytes < 0) {
@@ -129,7 +171,7 @@ exit(1);
 }
 /* Process data */
 
-if(!strcmp(data, QUIT_STRING)) want_quit = 1;
+/*if(!strcmp(data, QUIT_STRING)) want_quit = 1;
 else add_to_list(data);
 }
 list_flush();
@@ -186,3 +228,35 @@ if (server) start_server();
 else start_client(count);
 return 0;
 }
+
+*/
+/*int main(void)
+{
+modbus_t *cxt
+uint16_t tab_reg[64];
+int rc;
+int i;
+ctx = modbus_new_tcp("140.159.153.159", 1502);
+if(modbus_connect(ctx) == -1) {
+fprintf(stderr, "connection fail %s\n", modbus_strerror(errno));
+modbus_free(ctx);
+return -1;
+}
+
+rc = modbus_read_registers(ctx, 0, 10, tab_reg);
+if(rc == -1){
+fprintf(stderr, "%s\n/", modbus_strerror(errno));
+return -1;
+}
+
+for (i=0; i< rc; i++)
+{
+printf("reg[%d]=%d (0x%x)\n", i, tab_reg[i], tab_reg[i]);
+} modbus_close(ctx);
+modbus_free(ctx);
+}
+
+
+}*/
+
+
