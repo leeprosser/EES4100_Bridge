4528d068ff87378cf37c2814d88db42b3e279421
leeprosser <lee.prosser@live.vu.edu.au>
2015-09-17 05:33:08 +1000

    bacnet working one reg from one thread

--- src/hello_world.c
+++ src/hello_world.c
@@ -1,72 +1,206 @@
-/*
- * * * hello_world.c
- * * * Copyright (c) by Lee Prosser
+
+/*_world.c
+ * * Copyright (c) by Lee Prosser
  * * * Kim Taylor
  * * * Stéphane Raimbault
- * * <stephane.raimbault@gmail.com[1]>
- * *
- * * * This code is based on the code from Libmodbus Manual.
- * * *
- * * * This program is free software; you can redistribute it and/or modify
- * * * it under the terms of the GNU General Public License as published by
- * * * the Free Software Foundation; either version 2 of the License, or
- * * * (at your option) any later version.
- * * *
- * * * This program is distributed in the hope that it will be useful,
- * * * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * * * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
- * * * GNU General Public License for more details.
- * * *
- * * * You should have received a copy of the GNU General Public License
- * * * along with this program; if not, write to the Free Software
- * * * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA 02111-1307 USA
- * * *
- * * *
- * * */
+ **        <stephane.raimbault@gmail.com[1]>
+ **
+ ** * This code is based on the code from Libmodbus Manual.
+ ** *
+ ** * This program is free software; you can redistribute it and/or modify
+ ** * it under the terms of the GNU General Public License as published by
+ ** * the Free Software Foundation; either version 2 of the License, or
+ ** * (at your option) any later version.
+ ** *
+ ** * This program is distributed in the hope that it will be useful,
+ ** * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ ** * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ ** * GNU General Public License for more details.
+ ** *
+ ** * You should have received a copy of the GNU General Public License
+ ** * along with this program; if not, write to the Free Software
+ ** * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA 02111-1307 USA
+ ** *
+ ** * 
+ ** */
 
-#include <stdio.h>
-#include <stdlib.h>
-#include <string.h>
-#include <getopt.h>
-#include <pthread.h>
-#include <sys/socket.h>
-#include <arpa/inet.h>
-#include <netinet/in.h>
-#include <modbus-tcp.h>
-#include <libbacnet/address.h>
-#include <libbacnet/device.h>
-#include <libbacnet/handlers.h>
-#include <libbacnet/datalink.h>
-#include <libbacnet/bvlc.h>
-#include <libbacnet/client.h>
-#include <libbacnet/txbuf.h>
-#include <libbacnet/tsm.h>
-#include <libbacnet/ai.h>
-#include "bacnet_namespace.h" 
-#include <errno.h>
-#include <unistd.h>
+ #include <stdio.h>
+ #include <modbus-tcp.h>
+ #include <libbacnet/address.h>
+ #include <libbacnet/device.h>
+ #include <libbacnet/handlers.h>
+ #include <libbacnet/datalink.h>
+ #include <libbacnet/bvlc.h>
+ #include <libbacnet/client.h>
+ #include <libbacnet/txbuf.h>
+ #include <libbacnet/tsm.h>
+ #include <libbacnet/ai.h>
+ #include "bacnet_namespace.h"
+ #include <modbus-tcp.h>
+ #include <errno.h>
+ #include <stdio.h>
+ #include <stdlib.h>
+ #include <unistd.h>
+ #include <pthread.h>
+ #include <string.h>
+ #include <modbus-tcp.h>
 
-#define SERVER_ADDR "127.0.0.1"
-#define SERVER_PORT 10000
-#define DATA_LENGTH 256
-#define QUIT_STRING "exit"
-#include <errno.h>
+ #define BACNET_INSTANCE_NO          120
+ #define BACNET_PORT                 0xBAC1
+ #define BACNET_INTERFACE            "lo"
+ #define BACNET_DATALINK_TYPE        "bvlc"
+ #define BACNET_SELECT_TIMEOUT_MS    1       /* ms */
+
+ #define RUN_AS_BBMD_CLIENT          1
+
+ #if RUN_AS_BBMD_CLIENT
+ #define BACNET_BBMD_PORT            0xBAC0      //BBMD broadcast management device
+ #define BACNET_BBMD_ADDRESS         "127.255.255.255"
+ #define BACNET_BBMD_TTL             90          //BBMD broadcast management device time to live
+ #endif
+
+ int16_t holding;
+ int16_t tab_reg[64];
+ int errno;
+ int i;
+ int rc;
+
+ typedef struct s_word_object word_object;
+ struct s_word_object {
+ // was ok int16_t *word;
+  	int16_t word;
+ 	word_object *next;
+ };
+ 
+ 
+  /* list_head: Shared between two threads, must be accessed with list_lock */
+ 
+ static word_object *list_head;
+ static pthread_mutex_t list_lock = PTHREAD_MUTEX_INITIALIZER;
+ static pthread_cond_t list_data_ready = PTHREAD_COND_INITIALIZER;
+ static pthread_cond_t list_data_flush = PTHREAD_COND_INITIALIZER;
+ 
+ static void add_to_list(int16_t word) {
+ 	word_object *last_object, *tmp_object;
+ 	//word_object last_object, tmp_object;
+ 	int16_t tmp_string;
+ 	tmp_object = malloc(sizeof(word_object));
+ 	tmp_string = word;
+ 	tmp_object->word = tmp_string;
+  	tmp_object->next = NULL;
+	pthread_mutex_lock(&list_lock);
+	if (list_head == NULL) {
+		list_head = tmp_object;
+ 	} 
+	else{
+/* Iterate through the linked list to find the last object */
+		last_object = list_head;
+		while (last_object->next){
+			last_object = last_object->next;
+		}
+/* Last object is now found, link in our tmp_object at the tail */
+		last_object->next = tmp_object;
+	}
+	pthread_mutex_unlock(&list_lock);
+	pthread_cond_signal(&list_data_ready);
+}
+
+static word_object *list_get_first(void) {
+	word_object *first_object;
+	first_object = list_head;
+	list_head = list_head->next;
+	return first_object;
+}
+
+static void *print_func(void *arg) {
+	//static int holding;
+	word_object *current_object;
+	fprintf(stderr, "Print thr starting\n");
+	while(1) {
+		pthread_mutex_lock(&list_lock);
+		while (list_head == NULL) {
+			pthread_cond_wait(&list_data_ready, &list_lock);
+		}
+		current_object = list_get_first();
+		pthread_mutex_unlock(&list_lock);
+		printf("Print thread: %d\n", current_object->word);
+		holding =  current_object->word;
+		printf("holding in function %d\n" , holding);
+		free(current_object);
+		printf("in thread %d\n",i);
+		pthread_cond_signal(&list_data_flush);
+		break;
+	}
+	printf("in thread\n");
+	//return arg;
+}
+
+static void list_flush(void) {
+	pthread_mutex_lock(&list_lock);
+	while (list_head != NULL) {
+		pthread_cond_signal(&list_data_ready);
+		pthread_cond_wait(&list_data_flush, &list_lock);
+	}
+	pthread_mutex_unlock(&list_lock);
+}
+#define INC_TIMER(reset, func)  \
+do {                    \
+if (!--timer) { \
+timer = reset;      \
+func();             \
+}                       \
+} while (0)
+
+static bacnet_object_functions_t server_objects[] = {
+{bacnet_OBJECT_DEVICE, NULL,
+  bacnet_Device_Count,
+  bacnet_Device_Index_To_Instance,
+  bacnet_Device_Valid_Object_Instance_Number,
+  bacnet_Device_Object_Name,
+  bacnet_Device_Read_Property_Local,
+  bacnet_Device_Write_Property_Local,
+  bacnet_Device_Property_Lists,
+  bacnet_DeviceGetRRInfo,
+  NULL, /* Iterator */
+  NULL, /* Value_Lists */
+  NULL, /* COV */
+  NULL, /* COV Clear */
+  NULL  /* Intrinsic Reporting */
+},
+
+{bacnet_OBJECT_ANALOG_INPUT,
+  bacnet_Analog_Input_Init,
+  bacnet_Analog_Input_Count,
+  bacnet_Analog_Input_Index_To_Instance,
+  bacnet_Analog_Input_Valid_Instance,
+  bacnet_Analog_Input_Object_Name,
+  bacnet_Analog_Input_Read_Property,
+  bacnet_Analog_Input_Write_Property,
+  bacnet_Analog_Input_Property_Lists,
+  NULL /* ReadRangeInfo */ ,
+  NULL /* Iterator */ ,
+  bacnet_Analog_Input_Encode_Value_List,
+  bacnet_Analog_Input_Change_Of_Value,
+  bacnet_Analog_Input_Change_Of_Value_Clear,
+  bacnet_Analog_Input_Intrinsic_Reporting},
+  {MAX_BACNET_OBJECT_TYPE}
+};
 
 int16_t tab_reg[64];
 int errno;
 int i;
 int rc;
-
 int modb(void){
+	printf("in function modbus\n");
 	modbus_t *ctx;
 	sleep(1);
 	ctx = modbus_new_tcp("127.0.0.1", 0xBAC0);
 	if (modbus_connect(ctx) == -1){
-		fprintf(stderr, "Connections failed: %s\n", modbus_strerror(errno));
+		fprintf(stderr, "Connection failed: %s\n", modbus_strerror(errno));
 		modbus_free(ctx);
 		return -1;
 	}
-	printf("connect not failed\n");
+	printf("conection not failed\n");
 	rc = modbus_read_registers(ctx, 0, 3, tab_reg);
 	if (rc == -1){
 		if (EMBMDATA==1){printf("too many requests\n");}
@@ -81,215 +215,136 @@ int modb(void){
 	modbus_close(ctx);
 	modbus_free(ctx);
 }
-/* ALL CODE BELOW IS FOR FUTURE CONFIGURATION */
 
-/* Linked list object*/
-
-typedef struct s_word_object word_object;
-struct s_word_object {
-	char *word;
-	word_object *next;
-};
-
-/* list_head: Shared between two threads, must be accessed with list_lock */
-
-static word_object *list_head;
-static pthread_mutex_t list_lock = PTHREAD_MUTEX_INITIALIZER;
-static pthread_cond_t list_data_ready = PTHREAD_COND_INITIALIZER;
-static pthread_cond_t list_data_flush = PTHREAD_COND_INITIALIZER;
-/* Add object to list*/
-
-static void add_to_list(char *word) {
-	word_object *last_object, *tmp_object;
-	char *tmp_string;
-
-/* Do all memory allocation outside of locking - strdup() and malloc() can
- * * * block */
-
-	tmp_object = malloc(sizeof(word_object));
-	tmp_string = strdup(word);
-/* Set up tmp_object outside of locking */
-
-	tmp_object->word = tmp_string;
-	tmp_object->next = NULL;
-	pthread_mutex_lock(&list_lock);
-	if (list_head == NULL) {
-/* The list is empty, just place our tmp_object at the head */
-
-		list_head = tmp_object;
-	}
-	else {
-/* Iterate through the linked list to find the last object */
-
-		last_object = list_head;
-		while (last_object->next) {
-			last_object = last_object->next;
-		}	
-/* Last object is now found, link in our tmp_object at the tail */
-
-		last_object->next = tmp_object;
-	}
-	pthread_mutex_unlock(&list_lock);
-	pthread_cond_signal(&list_data_ready);
+static void register_with_bbmd(void) {
+	#if RUN_AS_BBMD_CLIENT
+	bacnet_bvlc_register_with_bbmd(
+	bacnet_bip_getaddrbyname(BACNET_BBMD_ADDRESS),
+	htons(BACNET_BBMD_PORT),
+	BACNET_BBMD_TTL);
+	#endif
 }
 
-/* Retrieve the first object in the linked list. Note that this function must
- * * * be called with list_lock held */
-
-static word_object *list_get_first(void) {
-	word_object *first_object;
-	first_object = list_head;
-	list_head = list_head->next;
-	return first_object;
+static void minute_tick(void) {
+/* Expire addresses once the TTL has expired */
+	bacnet_address_cache_timer(60);
+/* Re-register with BBMD once BBMD TTL has expired */
+        register_with_bbmd();
+/* Update addresses for notification class recipient list 
+ *          * *      * Requred for INTRINSIC_REPORTING
+ *                   *           * bacnet_Notification_Class_find_recipient(); */
 }
 
-static void *print_func(void *arg) {
-	word_object *current_object;
-	fprintf(stderr, "Print thread starting\n");
-	while(1) {
-		pthread_mutex_lock(&list_lock);
-		while (list_head == NULL) {
-			pthread_cond_wait(&list_data_ready, &list_lock);
-		}
-		current_object = list_get_first();
-		pthread_mutex_unlock(&list_lock);
-/* printf() and free() can block, make sure that we've released
- * * * list_lock first */
+#define S_PER_MIN 60
 
-		printf("Printing thread: %s\n", current_object->word);
-		free(current_object->word);
-		free(current_object);
-/* Let list_flush() know that we've done some work */
-
-		pthread_cond_signal(&list_data_flush);
-	}
-/* Silence compiler warning */
-	return arg;
+static void second_tick(void) {
+	static int timer = S_PER_MIN;
+/* Invalidates stale BBMD foreign device table entries */
+      	bacnet_bvlc_maintenance_timer(1);
+/* Transaction state machine: Responsible for retransmissions and ack
+ *         *      * checking for confirmed services */
+        bacnet_tsm_timer_milliseconds(1000);
+/* Re-enables communications after DCC_Time_Duration_Seconds
+*         *      * Required for SERVICE_CONFIRMED_DEVICE_COMMUNICATION_CONTROL
+*                 *           * bacnet_dcc_timer_seconds(1); */
+ /* State machine for load control object
+*         *      * Required for OBJECT_LOAD_CONTROL
+*                 *           * bacnet_Load_Control_State_Machine_Handler(); */
+ /* Expires any COV subscribers that have finite lifetimes
+ *      * Required for SERVICE_CONFIRMED_SUBSCRIBE_COV
+*                 *           * bacnet_handler_cov_timer_seconds(1); */
+/* Monitor Trend Log uLogIntervals and fetch properties
+*         *      * Required for OBJECT_TRENDLOG
+*                 *           * bacnet_trend_log_timer(1); */
+/* Run [Object_Type]_Intrinsic_Reporting() for all objects in device
+*         *      * Required for INTRINSIC_REPORTING
+*                 *           * bacnet_Device_local_reporting(); */
+	INC_TIMER(S_PER_MIN, minute_tick);
 }
 
-static void list_flush(void) {
-	pthread_mutex_lock(&list_lock);
-	while (list_head != NULL) {
-		pthread_cond_signal(&list_data_ready);
-		pthread_cond_wait(&list_data_flush, &list_lock);
-	}
-	pthread_mutex_unlock(&list_lock);
+#define MS_PER_SECOND 1000
+
+static void ms_tick(void) {
+        static int timer = MS_PER_SECOND;
+ /* Updates change of value COV subscribers.
+*          *      * Required for SERVICE_CONFIRMED_SUBSCRIBE_COV
+*                   *           * bacnet_handler_cov_task(); */
+        INC_TIMER(MS_PER_SECOND, second_tick);
 }
 
-static void start_server(void) {
-	int socket_fd;
-	struct sockaddr_in server_address;
-	struct sockaddr_in client_address;
-	socklen_t client_address_len;
-	int want_quit = 0;
-	fd_set read_fds;
-	int bytes;
-	char data[DATA_LENGTH];
-	pthread_t print_thread;
-	fprintf(stderr, "Starting server\n");
-	pthread_create(&print_thread, NULL, print_func, NULL);
-	socket_fd = socket(AF_INET, SOCK_DGRAM, 0);
-	memset(&server_address, 0, sizeof(server_address));
-	server_address.sin_family = AF_INET;
-	server_address.sin_port = htons(SERVER_PORT);
-	server_address.sin_addr.s_addr = INADDR_ANY;
-	if (bind(socket_fd, (struct sockaddr *) &server_address,
-	sizeof(server_address)) < 0) {
-		fprintf(stderr, "Bind failed\n");
-		exit(1);
-	}
-	FD_ZERO(&read_fds);
-	FD_SET(socket_fd, &read_fds);
-	while (!want_quit) {
-
-		if (select(socket_fd + 1, &read_fds, NULL, NULL, NULL) < 0) {
-			fprintf(stderr, "Select failed\n");
-			exit(1);
-		}
-		if (!FD_ISSET(socket_fd, &read_fds)) continue;
-/* Read input data */
-
-		bytes = recvfrom(socket_fd, data, sizeof(data), 0,
-		(struct sockaddr *) &client_address,
-		&client_address_len);
-		if (bytes < 0) {
-			fprintf(stderr, "Recvfrom failed\n");
-			exit(1);
-		}
-/* Process data */
-		if(!strcmp(data, QUIT_STRING)) want_quit = 1;
-		else add_to_list(data);
-	}
-	list_flush();
-}
-
-static void start_client(int count) {
-	int sock_fd;
-	struct sockaddr_in addr;
-	char input_word[DATA_LENGTH];
-	fprintf(stderr, "Accepting %i input strings\n", count);
-	if ((sock_fd = socket(AF_INET, SOCK_DGRAM, 0)) < 0) {
-		fprintf(stderr, "Socket failed\n");
-		exit(1);
-	}
-	memset(&addr, 0, sizeof(addr));
-	addr.sin_family = AF_INET;
-	addr.sin_port = htons(SERVER_PORT);
-	addr.sin_addr.s_addr = inet_addr(SERVER_ADDR);
-	if (connect(sock_fd, (struct sockaddr *) &addr, sizeof(addr)) < 0) {
-		fprintf(stderr, "Connect failed\n");
-		exit(1);
-	}
-	while (scanf("%256s", input_word) != EOF) {
-	if (send(sock_fd, input_word, strlen(input_word) + 1, 0) < 0) {
-		fprintf(stderr, "Send failed\n");
-		exit(1);
-	}
-	if (!--count) break;
-	}
-}
-
-
+#define BN_UNC(service, handler) \
+bacnet_apdu_set_unconfirmed_handler(                \
+SERVICE_UNCONFIRMED_##service,      \
+bacnet_handler_##handler)
+#define BN_CON(service, handler) \
+bacnet_apdu_set_confirmed_handler(                  \
+SERVICE_CONFIRMED_##service,        \
+bacnet_handler_##handler)
 
 int main(int argc, char **argv) {
-	printf("hi there\n");
-//modb();
-	while(1){modb();}
-	printf("working ok\n");
+        printf("in function main\n");
+	char input_word[256];
 	int c;
 	int option_index = 0;
 	int count = -1;
-	int server = 0;
-	static struct option long_options[] = {
-		{"count", required_argument, 0, 'c'},
-		{"server", no_argument, 0, 's'},
-		{0, 0, 0, 0 }
-	};
+	pthread_t print_thread;
+ /*static struct option long_options[] = {
+  * {"count", required_argument, 0, 'c'},
+  * {0, 0, 0, 0 }
+  *  };
+  * while (1) {
+  * c = getopt_long(argc, argv, "c:", long_options, &option_index);
+  *if (c == -1)
+  *break;
+  *switch (c) {
+  *case 'c':
+  *count = atoi(optarg);
+  *break;
+  * }
+  * }*/
+
+	uint8_t rx_buf[bacnet_MAX_MPDU];
+        uint16_t pdu_len;
+	BACNET_ADDRESS src;
+	//modb();
+	bacnet_Device_Set_Object_Instance_Number(BACNET_INSTANCE_NO);
+	bacnet_address_init();
+	   /* Setup device objects */
+	bacnet_Device_Init(server_objects);
+	BN_UNC(WHO_IS, who_is);
+	BN_CON(READ_PROPERTY, read_property);
+	bacnet_BIP_Debug = true;
+	bacnet_bip_set_port(htons(BACNET_PORT));
+	bacnet_datalink_set(BACNET_DATALINK_TYPE);
+	bacnet_datalink_init(BACNET_INTERFACE);
+	atexit(bacnet_datalink_cleanup);
+	memset(&src, 0, sizeof(src));
+	register_with_bbmd();
+	bacnet_Send_I_Am(&bacnet_Handler_Transmit_Buffer[0]);
+	//      while(1){modb();sleep(5); break;}
+	bacnet_Analog_Input_Present_Value_Set(0, holding);
+	printf("before while ever func main\n");
 	while (1) {
-		c = getopt_long(argc, argv, "c:s", long_options, &option_index);
-		if (c == -1)
-			break;
-		switch (c) {
-			case 'c':
-			count = atoi(optarg);
-			break;
-			case 's':
-			server = 1;
-			break;
-		}	
-	}	
-	if (server) start_server();
-	else{// while(1) modb();}
-	start_client(count);}
-	return 0;
+		pdu_len = bacnet_datalink_receive(
+		&src, rx_buf, bacnet_MAX_MPDU, BACNET_SELECT_TIMEOUT_MS);
+		if (pdu_len) bacnet_npdu_handler(&src, rx_buf, pdu_len);
+		ms_tick();
+		modb();
+		add_to_list(tab_reg[1]);
+
+		pthread_create(&print_thread, NULL, print_func, NULL);
+		bacnet_Analog_Input_Present_Value_Set(0, holding);
+                add_to_list(tab_reg[0]);
+                pthread_create(&print_thread, NULL, print_func, NULL);
+                bacnet_Analog_Input_Present_Value_Set(0, holding);
+                printf("holding %d\n", holding);
+                 //****    causing the program to stop!!!   list_flush();
+                //add_to_list(tab_reg[0]);
+               //add_to_list(tab_reg[0]);
+                printf("after list flush\n");
+                //add_to_list(tab_reg[1]);
+                //add_to_list(tab_reg[2]);
+        }
+        return 0;
 }
-
-
-
-
-
-
-
-
-
 
