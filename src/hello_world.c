3d97581f1b6a83100f34449299594c12b3977835
leeprosser <lee.prosser@live.vu.edu.au>
2015-09-25 06:53:49 +1000

    with bacnet_server / hello_world switch

--- src/hello_world.c
+++ src/hello_world.c
@@ -44,10 +44,16 @@
 
 
 #define BACNET_INSTANCE_NO          30
+//#define BACNET_INSTANCE_NO          12  // for testing at home
+
+
 #define BACNET_PORT                 0xBAC1
 #define BACNET_INTERFACE            "lo"
 #define BACNET_DATALINK_TYPE        "bvlc"
 #define BACNET_SELECT_TIMEOUT_MS    1       /* ms */
+
+#define RUN_AS_TESTBENCH            0
+#define RUN_AS_HELLO_WORLD          1
 
 #define RUN_AS_BBMD_CLIENT          1
 
@@ -57,6 +63,12 @@
 #define BACNET_BBMD_ADDRESS         "127.0.0.1"
 #define BACNET_BBMD_TTL             90          //BBMD broadcast management device time to live
 #endif
+
+static uint16_t test_data[] = {
+0xA4EC,  0x6E39, 0x8740, 0x1065, 0x9134, 0xFC8C };
+#define NUM_TEST_DATA (sizeof(test_data)/sizeof(test_data[0]))
+static pthread_mutex_t timer_lock = PTHREAD_MUTEX_INITIALIZER;
+
 
 int16_t holding; // the variable that is passed from the thread 
 //int16_t tab_reg[64]; // was used in modbus testbench
@@ -149,6 +161,28 @@ func();                         \
 }                               \
 } while (0)
 
+static int Update_Analog_Input_Read_Property(
+	BACNET_READ_PROPERTY_DATA *rpdata) {
+	static int index;
+        /* Update the values to be sent to the BACnet client here.
+	* The data should be read from the tail of a linked list. You are required
+	* to implement this list functionality.
+	
+	* bacnet_Analog_Input_Present_Value_Set() 
+	First argument: Instance No
+	Second argument: data to be sent
+	Without reconfiguring libbacnet, a maximum of 4 values may be sent */
+	
+	bacnet_Analog_Input_Present_Value_Set(0, test_data[index++]);
+	//bacnet_Analog_Input_Present_Value_Set(1, test_data[index++]);
+	/* bacnet_Analog_Input_Present_Value_Set(2, test_data[index++]); */
+
+	if (index == NUM_TEST_DATA) index = 0;
+	return bacnet_Analog_Input_Read_Property(rpdata);
+}
+
+
+
 static bacnet_object_functions_t server_objects[] = {
 {bacnet_OBJECT_DEVICE, NULL,
   bacnet_Device_Count,
@@ -225,6 +259,78 @@ static void register_with_bbmd(void) {
 	#endif
 }
 
+#if RUN_AS_TESTBENCH
+
+static void *minute_tick(void *arg) {
+    while (1) {
+    	pthread_mutex_lock(&timer_lock);
+        /* Expire addresses once the TTL has expired */
+        bacnet_address_cache_timer(60);
+        /* Re-register with BBMD once BBMD TTL has expired */
+        register_with_bbmd();
+        /* Update addresses for notification class recipient list 
+         * Requred for INTRINSIC_REPORTING
+         * bacnet_Notification_Class_find_recipient(); */
+        /* Sleep for 1 minute */
+        pthread_mutex_unlock(&timer_lock);
+        sleep(60);
+        }
+    return arg;
+}
+
+static void *second_tick(void *arg) {
+	while (1) {
+    		pthread_mutex_lock(&timer_lock);
+        	/* Invalidates stale BBMD foreign device table entries */
+        	bacnet_bvlc_maintenance_timer(1);
+        	/* Transaction state machine: Responsible for retransmissions and ack
+         	* checking for confirmed services */
+        	bacnet_tsm_timer_milliseconds(1000);
+       		/* Re-enables communications after DCC_Time_Duration_Seconds
+        	* Required for SERVICE_CONFIRMED_DEVICE_COMMUNICATION_CONTROL
+       	 	* bacnet_dcc_timer_seconds(1); */
+       		/* State machine for load control object
+        	* Required for OBJECT_LOAD_CONTROL
+        	* bacnet_Load_Control_State_Machine_Handler(); */
+       		/* Expires any COV subscribers that have finite lifetimes
+        	* Required for SERVICE_CONFIRMED_SUBSCRIBE_COV
+        	* bacnet_handler_cov_timer_seconds(1); */
+       		/* Monitor Trend Log uLogIntervals and fetch properties
+        	* Required for OBJECT_TRENDLOG
+        	* bacnet_trend_log_timer(1); */
+       		/* Run [Object_Type]_Intrinsic_Reporting() for all objects in device
+        	* Required for INTRINSIC_REPORTING
+        	* bacnet_Device_local_reporting(); */
+       		/* Sleep for 1 second */
+		pthread_mutex_unlock(&timer_lock);
+        	sleep(1);
+	}
+	return arg;
+}
+
+static void ms_tick(void) {
+    /* Updates change of value COV subscribers.
+    * Required for SERVICE_CONFIRMED_SUBSCRIBE_COV
+    * bacnet_handler_cov_task(); */
+}
+
+#define BN_UNC(service, handler) \
+bacnet_apdu_set_unconfirmed_handler(\
+SERVICE_UNCONFIRMED_##service,      \
+bacnet_handler_##handler)
+#define BN_CON(service, handler) \
+bacnet_apdu_set_confirmed_handler(                  \
+SERVICE_CONFIRMED_##service,        \
+bacnet_handler_##handler)
+
+
+#endif
+
+
+
+
+#if RUN_AS_HELLO_WORLD
+
 static void minute_tick(void) {
 /* Expire addresses once the TTL has expired */
 	bacnet_address_cache_timer(60);
@@ -272,6 +378,8 @@ static void ms_tick(void) {
         INC_TIMER(MS_PER_SECOND, second_tick);
 }
 
+
+
 #define BN_UNC(service, handler) \
 bacnet_apdu_set_unconfirmed_handler(                \
 SERVICE_UNCONFIRMED_##service,      \
@@ -280,6 +388,8 @@ bacnet_handler_##handler)
 bacnet_apdu_set_confirmed_handler(                  \
 SERVICE_CONFIRMED_##service,        \
 bacnet_handler_##handler)
+
+#endif
 
 int main(int argc, char **argv) {
         printf("in function main\n"); //for diagnostics
@@ -291,7 +401,11 @@ int main(int argc, char **argv) {
 	uint8_t rx_buf[bacnet_MAX_MPDU];
         uint16_t pdu_len;
 	BACNET_ADDRESS src;
-	//modb();
+
+#if RUN_AS_TESTBENCH
+
+	pthread_t minute_tick_id, second_tick_id;   // bacnet testbench
+#endif
 	bacnet_Device_Set_Object_Instance_Number(BACNET_INSTANCE_NO);
 	bacnet_address_init();
 	   /* Setup device objects */
@@ -308,6 +422,14 @@ int main(int argc, char **argv) {
 	bacnet_Send_I_Am(&bacnet_Handler_Transmit_Buffer[0]);
 	printf("before while ever func main\n");
 	pthread_create(&print_thread, NULL, print_func, NULL);
+
+	
+#if RUN_AS_TESTBENCH	
+
+	bacnet_Send_I_Am(bacnet_Handler_Transmit_Buffer);   //run as testbench
+	pthread_create(&minute_tick_id, 0, minute_tick, NULL);
+        pthread_create(&second_tick_id, 0, second_tick, NULL);
+#endif
 
 
 	printf("in function modbus\n");
@@ -333,7 +455,24 @@ int main(int argc, char **argv) {
 
 	while (1) {
 
-    		rc = modbus_read_registers(ctx, 30, 3, tab_reg);
+    #if RUN_AS_TESTBENCH	 
+    		 printf("NO GOOD !!\n");
+		 pdu_len = bacnet_datalink_receive(                            //run as testbench
+		 &src, rx_buf, bacnet_MAX_MPDU, BACNET_SELECT_TIMEOUT_MS);
+	         if (pdu_len) {
+			/* May call any registered handler.
+			* Thread safety: May block, however we still need to guarantee
+			* atomicity with the timers, so hold the lock anyway */
+			pthread_mutex_lock(&timer_lock);
+			bacnet_npdu_handler(&src, rx_buf, pdu_len);
+			pthread_mutex_unlock(&timer_lock);
+		 }
+                ms_tick();  
+    #endif
+	
+		
+		
+		rc = modbus_read_registers(ctx, 30, 3, tab_reg);
 		//rc = modbus_read_registers(ctx, 0, 3, tab_reg); // was used for testbench modbus server
 		for (i=0; i < rc; i++){
 			sleep(1);
